d0937a69cc436edb62a0c895b650e51de0bfebc2
Samson Ameh <amehsam69@gmail.com>
2023-11-18 03:38:51 +0100

    Conversion completed

--- index.js
+++ index.js
@@ -3,3 +3,24 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const lengthEl = document.querySelector(".length-el");
+const volumeEl = document.querySelector(".volume-el");
+const massEl = document.querySelector(".mass-el");
+const convertBtn = document.querySelector("#convert-btn");
+let inputEl = document.querySelector("#number-to-convert");
+
+convertBtn.addEventListener("click", () => {
+  let inputData = Number(inputEl.value);
+  lengthEl.textContent = `${inputData} meters =  ${
+    inputData * 3.281
+  } feet | ${inputData} feet = ${inputData * 0.3048} meters`;
+
+  volumeEl.textContent = `${inputData} liters =  ${
+    inputData * 0.264
+  } gallon | ${inputData} gallons = ${inputData * 3.785411784}  liters`;
+
+  massEl.textContent = `${inputData} kilos =  ${
+    inputData * 2.204
+  } pounds | ${inputData} pounds = ${inputData * 0.45359237}  kilos`;
+});
