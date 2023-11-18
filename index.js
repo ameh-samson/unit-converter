e52dac0a22d0e1871b1da0f5c304449972503b2b
Samson Ameh <amehsam69@gmail.com>
2023-11-18 04:15:00 +0100

    The event was refactored

--- index.js
+++ index.js
@@ -12,15 +12,26 @@ let inputEl = document.querySelector("#number-to-convert");
 
 convertBtn.addEventListener("click", () => {
   let inputData = Number(inputEl.value);
-  lengthEl.textContent = `${inputData} meters =  ${
-    inputData * 3.281
-  } feet | ${inputData} feet = ${inputData * 0.3048} meters`;
-
-  volumeEl.textContent = `${inputData} liters =  ${
-    inputData * 0.264
-  } gallon | ${inputData} gallons = ${inputData * 3.785411784}  liters`;
-
-  massEl.textContent = `${inputData} kilos =  ${
-    inputData * 2.204
-  } pounds | ${inputData} pounds = ${inputData * 0.45359237}  kilos`;
+  if (isNaN(inputData)) {
+    alert("Enter a valid number");
+    return;
+  }
+  convert(inputData);
 });
+
+function convert(inputData) {
+  const convertMetersToFeet = (inputData * 3.281).toFixed(2);
+  const convertFeetToMeters = (inputData * 0.3048).toFixed(2);
+
+  const convertLitersTGallon = (inputData * 0.264).toFixed(2);
+  const convertGallonToLiters = (inputData * 3.785411784).toFixed(2);
+
+  const convertKilosToPounds = (inputData * 2.204).toFixed(2);
+  const convertPoundsToKilos = (inputData * 0.45359237).toFixed(2);
+
+  lengthEl.textContent = `${inputData} meters = ${convertMetersToFeet} feet || ${inputData} feet = ${convertFeetToMeters} meters`;
+  volumeEl.textContent = `${inputData} liters = ${convertLitersTGallon} gallon || ${inputData} gallon = ${convertGallonToLiters} liters`;
+  massEl.textContent = `${inputData} kilos = ${convertKilosToPounds} pounds || ${inputData} pound = ${convertPoundsToKilos} kilos`;
+
+  return inputData.toFixed(2);
+}
